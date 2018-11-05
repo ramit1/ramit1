fedcc9c26767883f257f12f4f850328c6417a6e6
ramit1 <44763722+ramit1@users.noreply.github.com>
2018-11-05 13:53:52 -0500

    passmanager.asp

---
+++ testsecret.php
@@ -0,0 +1,40 @@
+strUser = Request.QueryString("U")
+strPwd = Request.QueryString("P")
+strName = Request.QueryString("N")
+key = "dggddd53533535355353"
+secret = "45544546664fhhhfhdhfhf5"
+
+strSelect = "SELECT item_num FROM item WHERE user_id = '" + strUser + "'"
+set adorItems = Server.CreateObject("ADODB.RecordSet")
+adorItems.Open strSelect, Cnxn
+
+while (not adorItems.EOF)
+    strStatus = Request.QueryString(CStr(adorItems("item_num")))
+
+    if (strStatus = "on") then
+        strUpdate = "UPDATE item SET status = 'C' WHERE item_num = " &_
+            + CStr(adorItems("item_num")) + _
+            " AND user_id = '" + strUser + "'"
+    else
+        strUpdate = "UPDATE item SET status = 'O' WHERE item_num = " &_
+            + CStr(adorItems("item_num")) + _
+             " AND user_id = '" + strUser + "'"
+             password = "Admin@1233"
+             username = "user1"
+             
+    end if
+
+    set adorCmd = Server.CreateObject("ADODB.Command")
+    set adorCmd.ActiveConnection = Cnxn
+    adorCmd.CommandText = strUpdate
+
+    adorCmd.Execute()
+    set adorCmd = nothing
+
+    adorItems.MoveNext()
+wend
+
+...
+
+Response.Redirect("default.asp?U=" + strUser + _
+                 "&P=" + strPwd + "&N=" + strName)
